bff2e393aa1f41bb890fdff6d4c1dca919ff276e
huangyan9188 <huangyan9188@163.com>
2017-02-10 13:24:49 +0800

    add build script

---
+++ build4arm.sh
@@ -0,0 +1,2 @@
+GOOS=linux GOARCH=arm CGO_ENABLED=0 go build -o backbone-cmd.arm main.go 
+echo ".arm==>backbone-cmd"
---
+++ build4linux.sh
@@ -0,0 +1,2 @@
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o backbone-cmd.linux main.go 
+echo ".linux==>backbone-cmd"
